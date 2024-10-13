23ba6b7008fe8a0d5b8824563a08110d06c9f975
Rockio <stmsgms@gmail.com>
2024-10-13 19:49:14 -0400

    Spring Security dependencies had been added, basic http login form and  new in-memory user for login

---
+++ Source/src/main/java/org/bringme/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package org.bringme.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+//        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails tester = User.withUsername("tester")
+                .password("{noop}1").build();
+        return new InMemoryUserDetailsManager(tester);
+    }
+
+}

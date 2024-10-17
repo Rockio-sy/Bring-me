79439f48377310795075f22d2f4ab7e4cb079012
Rockio <stmsgms@gmail.com>
2024-10-17 05:31:22 -0400

    Validate token had been done

--- Source/src/main/java/org/bringme/security/SecurityConfig.java
+++ Source/src/main/java/org/bringme/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.bringme.security;
 
 
+import org.bringme.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,15 +16,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final UserDetailsService userDetailService;
+    private final JwtFilter jwtFilter;
 
-    public SecurityConfig(UserDetailsService userDetailService) {
+    public SecurityConfig(UserDetailsService userDetailService, JwtFilter jwtFilter) {
         this.userDetailService = userDetailService;
+        this.jwtFilter = jwtFilter;
     }
 
     @Bean
@@ -37,6 +41,7 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
--- Source/src/main/java/org/bringme/service/impl/JwtService.java
+++ Source/src/main/java/org/bringme/service/impl/JwtService.java
@@ -1,15 +1,19 @@
 package org.bringme.service.impl;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -20,23 +24,64 @@ public class JwtService {
         this.secretKey = secretKey;
     }
 
-    public String generateToken(Long id) {
+
+    public String generateToken(Long id, String emailOrPhone) {
         Map<String, Object> claims = new HashMap<>();
 
         return Jwts.builder()
                 .claims()
                 .add(claims)
                 .id(Long.toString(id))
+                .subject(emailOrPhone)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() * 1000 * 60 * 60 * 10))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .and()
                 .signWith(getKey())
                 .compact();
 
     }
 
-    private Key getKey(){
-        return Keys.hmacShaKeyFor(secretKey.getBytes());
 
+    /**
+     * After adding username to the application, we can verify the token by username only without,
+     * needing to use @emailOrPhone
+     */
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String emailOrPhone = extractEmailOrPhone(token);
+        return (emailOrPhone.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private SecretKey getKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String extractId(String token) {
+        return extractClaim(token, Claims::getId);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getKey())
+                .build().parseSignedClaims(token).getPayload();
+    }
+
+    public String extractEmailOrPhone(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }

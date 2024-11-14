6dba768645bf0517658db7d16439513544202435
Rockio <stmsgms@gmail.com>
2024-11-14 20:53:41 +0300

    Authenticatoin error has been fixed

--- Source/src/main/java/org/bringme/service/impl/MyUserDetailService.java
+++ Source/src/main/java/org/bringme/service/impl/MyUserDetailService.java
@@ -3,6 +3,9 @@ package org.bringme.service.impl;
 import org.bringme.model.AuthUserDetails;
 import org.bringme.model.Person;
 import org.bringme.repository.AuthRepository;
+import org.bringme.service.exceptions.CustomException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,11 +23,15 @@ public class MyUserDetailService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String emailOrPhone) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String emailOrPhone) throws UsernameNotFoundException, CustomException{
         Optional<Person> user = authRepository.getByEmailOrPhone(emailOrPhone);
 
         if (user.isEmpty()) {
-            throw new UsernameNotFoundException("Invalid input");
+            throw new CustomException("User nor found", HttpStatus.UNAUTHORIZED);
+        }
+
+        if(user.get().getAccountStatus() > 1){
+            throw new CustomException("Account is locked", HttpStatus.LOCKED);
         }
 
         return new AuthUserDetails(user.get());

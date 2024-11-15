ec2410d6234b388203d4d451a81eea08a573b356
Rockio <stmsgms@gmail.com>
2024-11-15 15:15:41 +0300

    Data Seeder has been modfied to get the id automatically

--- Source/src/main/java/org/bringme/config/DataSeeder.java
+++ Source/src/main/java/org/bringme/config/DataSeeder.java
@@ -51,9 +51,9 @@ public class DataSeeder implements CommandLineRunner {
                 String sql = "INSERT INTO trips (origin, destination, destination_airport, empty_weight, departure_time, arrival_time, transit, comments, passenger_id) " +
                         "VALUES " +
                         "(1, 2, 'Airport', 2, (NOW() + INTERVAL '2 years' + INTERVAL '1 day')::TIMESTAMP, (NOW() + INTERVAL '2 years' + INTERVAL '1 day')::TIMESTAMP, " +
-                        "true, 'seed comment', 15), " +
+                        "true, 'seed comment', (SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com'))," +
                         "(3, 4, 'Airport', 2, (NOW() + INTERVAL '2 years' + INTERVAL '1 day')::TIMESTAMP, (NOW() + INTERVAL '2 years' + INTERVAL '1 day')::TIMESTAMP, " +
-                        "true, 'seed comment', 15)";
+                        "true, 'seed comment', (SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com'))";
 
                 jdbcTemplate.update(sql);
                 System.out.println("Development trips data has been seeded.");
@@ -70,9 +70,9 @@ public class DataSeeder implements CommandLineRunner {
             Long count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM items", Long.class);
             if (count == null || count == 0) {
                 String sql = "INSERT INTO items (name, origin, destination, user_id, weight, height, length, full_address, comments, photo)" +
-                        "VALUES" +
-                        "('items1', 1, 2, 16, 1, 1, 1, 'Full Address seed', 'seed comment', 'ITEM_item1-3e3ffef9-0754-4926-99f8-eee363b57def-1729650538812-.png')," +
-                        "('items2', 3, 4, 16, 1, 1, 1, 'Full Address seed', 'seed comment', 'ITEM_item1-0669716b-9a26-4348-950b-9d5584b9910d-1729576564685-.png')";
+                        "VALUES " +
+                        "('items1', 1, 2, (SELECT id FROM persons WHERE email = 'tarekshaweshph2@gmail.com'), 1, 1, 1, 'Full Address seed', 'seed comment', 'ITEM_item1-3e3ffef9-0754-4926-99f8-eee363b57def-1729650538812-.png')," +
+                        "('items2', 3, 4, (SELECT id FROM persons WHERE email = 'tarekshaweshph2@gmail.com'), 1, 1, 1, 'Full Address seed', 'seed comment', 'ITEM_item1-0669716b-9a26-4348-950b-9d5584b9910d-1729576564685-.png')";
                 jdbcTemplate.update(sql);
                 System.out.println("Development items data has been seeded.");
             } else {
@@ -88,9 +88,22 @@ public class DataSeeder implements CommandLineRunner {
             Long count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM requests", Long.class);
             if (count == null || count == 0) {
                 String sql = "INSERT INTO requests(requester_user_id, requested_user_id, item_id, trip_id, origin, destination, comments, price)" +
-                        "VALUES" +
-                        "(15, 16, 22, 19, 1, 2, 'Seed comment', 2.2)," +
-                        "(16, 15, 23, 20, 3, 4, 'Seed comment', 2.2)";
+                        "VALUES " +
+                        "((SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com'), " +
+                        "(SELECT id FROM persons WHERE email = 'tarekshaweshph2@gmail.com'), " +
+                        "(SELECT id FROM items WHERE name = 'items1' AND user_id = (SELECT id FROM persons WHERE email = 'tarekshaweshph2@gmail.com')), " +
+                        "(SELECT id FROM trips WHERE passenger_id = (SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com') LIMIT 1), " +
+                        "(SELECT origin FROM trips WHERE id = (SELECT id FROM trips WHERE passenger_id = (SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com') LIMIT 1)), " +
+                        "(SELECT destination FROM trips WHERE id = (SELECT id FROM trips WHERE passenger_id = (SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com') LIMIT 1)), " +
+                        "'Seed comment', 2.2)," +
+                        "((SELECT id FROM persons WHERE email = 'tarekshaweshph2@gmail.com'), " +
+                        "(SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com'), " +
+                        "(SELECT id FROM items WHERE name = 'items2' AND user_id = (SELECT id FROM persons WHERE email = 'tarekshaweshph2@gmail.com')), " +
+                        "(SELECT id FROM trips WHERE passenger_id = (SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com') OFFSET 1 LIMIT 1), " +
+                        "(SELECT origin FROM trips WHERE id = (SELECT id FROM trips WHERE passenger_id = (SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com') OFFSET 1 LIMIT 1)), " +
+                        "(SELECT destination FROM trips WHERE id = (SELECT id FROM trips WHERE passenger_id = (SELECT id FROM persons WHERE email = 'tarekshawesh23@gmail.com') OFFSET 1 LIMIT 1)), " +
+                        "'Seed comment', 2.2)";
+
                 jdbcTemplate.update(sql);
                 System.out.println("Development requests data has been seeded.");
             } else {
@@ -100,4 +113,6 @@ public class DataSeeder implements CommandLineRunner {
             System.out.println(e.getMessage());
         }
     }
+
+
 }

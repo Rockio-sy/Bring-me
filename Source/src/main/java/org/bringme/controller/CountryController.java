8fbb176666bbb37e75e05a7f7e8d83db172108b1
Rockio <stmsgms@gmail.com>
2024-11-17 02:28:59 +0300

    Get countries endpoint has been added

---
+++ Source/src/main/java/org/bringme/controller/CountryController.java
@@ -0,0 +1,27 @@
+package org.bringme.controller;
+
+import org.bringme.model.Country;
+import org.bringme.service.impl.CountryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    // Define the endpoint to get the list of countries
+    @GetMapping("/api/countries")
+    public List<Country> getCountries() {
+        // Call the service to get the list of countries
+        return countryService.getCountries();
+    }
+}

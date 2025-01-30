16925571e10bec90f5a622f5be595cccfc206c40
Rockio <stmsgms@gmail.com>
2025-01-30 09:35:32 +0300

    JWT filter class has been documentated

--- Source/src/main/java/org/bringme/filter/JwtFilter.java
+++ Source/src/main/java/org/bringme/filter/JwtFilter.java
@@ -19,18 +19,44 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+/**
+ * Filter for handling JWT authentication.
+ * <p>
+ * This filter intercepts incoming HTTP requests, extracts the JWT token from the "Authorization" header,
+ * validates the token, and sets the authentication details in the Spring Security context.
+ * </p>
+ */
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final ApplicationContext context;
 
-
+    /**
+     * Constructs a JwtFilter with dependencies.
+     *
+     * @param jwtService The service used to handle JWT operations (validation, extraction).
+     * @param context    The application context to retrieve user details.
+     */
     public JwtFilter(JwtService jwtService, ApplicationContext context) {
         this.jwtService = jwtService;
         this.context = context;
     }
 
+    /**
+     * Filters incoming requests to check for a valid JWT token.
+     * <p>
+     * This method extracts the token from the "Authorization" header, validates it,
+     * retrieves the user details, and sets the authentication in the SecurityContext.
+     * </p>
+     *
+     * @param request     The HTTP request.
+     * @param response    The HTTP response.
+     * @param filterChain The filter chain.
+     * @throws ServletException If a servlet-specific error occurs.
+     * @throws IOException      If an input or output error occurs.
+     * @throws CustomException  as response {@link org.bringme.controller.ControllerAdvice} for every exception handled.
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
